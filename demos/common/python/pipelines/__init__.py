74e30a6fe045202b7c55e02ce12c3be2308a1957
Pankratova <anzhella.pankratova@intel.com>
2021-03-09 14:04:05 +0300

    Add sync_two_stage_pipeline

--- demos/common/python/pipelines/__init__.py
+++ demos/common/python/pipelines/__init__.py
@@ -2,11 +2,13 @@
 from .async_pipeline import get_user_config, AsyncPipeline
 =======
 from .async_pipeline import AsyncPipeline
+from .sync_two_stage_pipeline import SyncTwoStagePipeline
 from .two_stage_pipeline import TwoStagePipeline
 >>>>>>> 8d598b424 (Add text_detection_demo and two_stage_pipeline)
 
 __all__ = [
     'get_user_config',
     'AsyncPipeline',
+    'SyncTwoStagePipeline',
     'TwoStagePipeline',
 ]
